0f3afd6ac8f7372bed7e6aea49017b8341a7ea41
hulaalol <marci.vfb4ever@googlemail.com>
2015-10-16 01:55:57 +0200

    Fixed Equipment Bug, recalculatoin of level by FOLLOW_RANGE generic attribute

--- CustomMobs3/src/de/hulaa/CustomMobs3/CustomEntitySkeleton.java
+++ CustomMobs3/src/de/hulaa/CustomMobs3/CustomEntitySkeleton.java
@@ -16,165 +16,118 @@ import net.minecraft.server.v1_8_R3.World;
 
 import java.lang.Integer;
 
-	
+public class CustomEntitySkeleton extends EntitySkeleton {
 
-	
-	public class CustomEntitySkeleton extends EntitySkeleton {
-		
-		//public int MobLevel;
-	
-		
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.minecraft.server.v1_8_R3.EntityLiving#aZ()
+	 */
+	@Override
+	protected void aZ() {
 
-		/* (non-Javadoc)
-		 * @see net.minecraft.server.v1_8_R3.EntityLiving#aZ()
-		 */
-		@Override
-		protected void aZ() {
-			
-			 ++this.deathTicks;
-		        if (this.deathTicks == 20) {
-		            int i;
+		++this.deathTicks;
+		if (this.deathTicks == 20) {
+			int i;
 
-		            if (!this.world.isClientSide && (this.lastDamageByPlayerTime > 0 || this.alwaysGivesExp()) && this.ba() && this.world.getGameRules().getBoolean("doMobLoot")) {
-		                i = this.getExpValue(this.killer);
+			if (!this.world.isClientSide
+					&& (this.lastDamageByPlayerTime > 0 || this
+							.alwaysGivesExp()) && this.ba()
+					&& this.world.getGameRules().getBoolean("doMobLoot")) {
+				i = this.getExpValue(this.killer);
 
-		                while (i > 0) {
-		                    int j = EntityExperienceOrb.getOrbValue(i);
+				while (i > 0) {
+					int j = EntityExperienceOrb.getOrbValue(i);
 
-		                    i -= j;
-		                    //this.world.addEntity(new EntityExperienceOrb(this.world, this.locX, this.locY, this.locZ, j));
-		                }
-		                
-		                World world = this.getWorld();
-		                org.bukkit.World BukkitWorld = world.getWorld();
-		                Location loc = new Location(BukkitWorld, this.locX, this.locY, this.locZ);
-		                
-		                for(int numberOfOrbs = 10; numberOfOrbs>0; numberOfOrbs--){
-		                	Bukkit.getServer().broadcastMessage("[BETA] Skeleton xp orb distribution");
-		                	Entity orb = new CustomEntityExperienceOrb(this.world);
-	 	                    AdditionalMobSpawner.spawnEntity(orb, loc );
-		                }
-		                
-		                
-		            }
-
-		            this.die();
-
-//		            for (i = 0; i < 20; ++i) {
-//		                double d0 = this.random.nextGaussian() * 0.02D;
-//		                double d1 = this.random.nextGaussian() * 0.02D;
-//		                double d2 = this.random.nextGaussian() * 0.02D;
-//
-//		                this.world.addParticle("explode", this.locX + (double) (this.random.nextFloat() * this.width * 2.0F) - (double) this.width, this.locY + (double) (this.random.nextFloat() * this.length), this.locZ + (double) (this.random.nextFloat() * this.width * 2.0F) - (double) this.width, d0, d1, d2);
-//		            }
-		        }
-			
-
-			
-//			++this.deathTicks;
-//			
-//	        if (this.deathTicks == 20) {
-//	           int i;
-//
-//	            if (!this.world.isClientSide && (this.lastDamageByPlayerTime > 0 || this.alwaysGivesExp()) && this.ba() && this.world.getGameRules().getBoolean("doMobLoot")) {
-//	            		
-//	            		i=this.getExpValue(this.killer);
-//	            		int numberOfOrbs = i;
-//	            		Bukkit.getServer().broadcastMessage("[BETA] Skeleton xp distribution");
-//	            		
-//	            		World world = this.getWorld();
-//	                    org.bukkit.World BukkitWorld = world.getWorld();
-//	                    Location loc = new Location (BukkitWorld, this.locX, this.locY, this.locZ);
-//	            		
-//	            		for(; numberOfOrbs>=0; numberOfOrbs--){
-//
-//	            			Entity orb = new CustomEntityExperienceOrb(this.world);
-//	 	                    AdditionalMobSpawner.spawnEntity(orb, loc );
-//	 	                    
-//	            		}
-//  
-//	                }
-//	            }
-//
-//	            this.die();
-//	            Bukkit.getServer().broadcastMessage("[BETA] Skeleton die()");
-//
-////	            for (int i = 0; i < 20; ++i) {
-////	                double d0 = this.random.nextGaussian() * 0.02D;
-////	                double d1 = this.random.nextGaussian() * 0.02D;
-////	                double d2 = this.random.nextGaussian() * 0.02D;
-////	                this.world.addParticle(EnumParticle.FIREWORKS_SPARK, this.locX + (double) (this.random.nextFloat() * this.width * 2.0F) - (double) this.width, this.locY + (double) (this.random.nextFloat() * this.length), this.locZ + (double) (this.random.nextFloat() * this.width * 2.0F) - (double) this.width, d0, d1, d2);
-////	            }
-	            
-	        }
-
-		
-
-		public CustomEntitySkeleton(World world) {
-		super(world);
-		
-		//EQUIPMENT PROCESS
-		//if(MobLevel>0){
-			ItemStack[] equipment = EquipmentGenerator.generateEquipment(11);
-			for (int i = 0; i<5; i++){
-				
-				if (equipment[i]!=null)
-				{
-					net.minecraft.server.v1_8_R3.ItemStack equip = CraftItemStack.asNMSCopy(equipment[i]);
-					super.setEquipment(i,equip );
+					i -= j;
+					// this.world.addEntity(new EntityExperienceOrb(this.world,
+					// this.locX, this.locY, this.locZ, j));
 				}
+
+				World world = this.getWorld();
+				org.bukkit.World BukkitWorld = world.getWorld();
+				Location loc = new Location(BukkitWorld, this.locX, this.locY,
+						this.locZ);
+
+				for (int numberOfOrbs = 10; numberOfOrbs > 0; numberOfOrbs--) {
+					Bukkit.getServer().broadcastMessage(
+							"[BETA] Skeleton xp orb distribution");
+					Entity orb = new CustomEntityExperienceOrb(this.world);
+					AdditionalMobSpawner.spawnEntity(orb, loc);
+				}
+
 			}
-			
-		//}else{
-			//Bukkit.getServer().broadcastMessage("[BETA] Skeleton Level was not greater 0 :(");
-			
-		//}
-		
-        super.dropEquipment(true, 1);
-		
-		
-//		Double level = ((this.getAttributeInstance(GenericAttributes.maxHealth).getValue()-5D)/1.5D);
-//		Integer intLevel = level.intValue();
-//
-//		
-//		String levelString = String.format("%d", (intLevel));
-//		super.setCustomName("StandardSkeleton <"+levelString+">");
-		
 
-}
+			this.die();
 
-		@Override
-		protected void initAttributes() {
-			super.initAttributes();
-			
-			
-
-			int level = AdditionalMobSpawner.spawnLevel();
-			//MobLevel = level;
-			this.setCustomName("Skeleton <"+level+">");
-
-			double attackDamage= (2D+(0.5D*level));
-			double maxHealth = CustomEntitySkeletonAttributeSetter.setSkeletonHealth(level);
-			double mvmnt = (0.1D*(level/8)+0.1D);
-			double followRange = (15D);
-			double KnockbackResist = 0.1D+(level/30);
-			
-
-
-			this.getAttributeInstance(GenericAttributes.maxHealth).setValue(maxHealth);
-			this.getAttributeInstance(GenericAttributes.ATTACK_DAMAGE).setValue(attackDamage);
-			this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(0.2D);
-			this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(15.0D);
-			this.getAttributeInstance(GenericAttributes.c).setValue(KnockbackResist);
+			// for (i = 0; i < 20; ++i) {
+			// double d0 = this.random.nextGaussian() * 0.02D;
+			// double d1 = this.random.nextGaussian() * 0.02D;
+			// double d2 = this.random.nextGaussian() * 0.02D;
+			//
+			// this.world.addParticle("explode", this.locX + (double)
+			// (this.random.nextFloat() * this.width * 2.0F) - (double)
+			// this.width, this.locY + (double) (this.random.nextFloat() *
+			// this.length), this.locZ + (double) (this.random.nextFloat() *
+			// this.width * 2.0F) - (double) this.width, d0, d1, d2);
+			// }
 		}
-		
-//		public double getLevel(){
-//			return ((this.getAttributeInstance(GenericAttributes.maxHealth).getValue()-5D)/1.5D);
-//		}
-	
 
-		
-		
-		
-	
+	}
+
+	public CustomEntitySkeleton(World world) {
+		super(world);
+
+		int level = this.getLevel();
+
+		// EQUIPMENT PROCESS
+
+		ItemStack[] equipment = EquipmentGenerator.generateEquipment(level);
+		for (int i = 0; i < 5; i++) {
+
+			if (equipment[i] != null) {
+				net.minecraft.server.v1_8_R3.ItemStack equip = CraftItemStack
+						.asNMSCopy(equipment[i]);
+				super.setEquipment(i, equip);
+			}
+		}
+
+		super.dropEquipment(true, 1);
+
+	}
+
+	@Override
+	protected void initAttributes() {
+		super.initAttributes();
+
+		int level = AdditionalMobSpawner.spawnLevel();
+
+		this.setCustomName("Skeleton <" + level + ">");
+
+		double attackDamage = (2D + (0.5D * level));
+		double maxHealth = CustomEntitySkeletonAttributeSetter
+				.setSkeletonHealth(level);
+		double mvmnt = (0.1D * (level / 8) + 0.1D);
+		double followRange = (15D);
+		double KnockbackResist = 0.1D + (level / 30);
+
+		this.getAttributeInstance(GenericAttributes.maxHealth).setValue(
+				maxHealth);
+		this.getAttributeInstance(GenericAttributes.ATTACK_DAMAGE).setValue(
+				attackDamage);
+		this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(
+				0.2D);
+		this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(
+				16.0D + level);
+		this.getAttributeInstance(GenericAttributes.c)
+				.setValue(KnockbackResist);
+	}
+
+	public int getLevel() {
+		Double level = ((this
+				.getAttributeInstance(GenericAttributes.FOLLOW_RANGE)
+				.getValue()) - 16.0D);
+		return level.intValue();
+	}
+
 }
